06dca54cc97e5eb7c819a9943a27a2bf700687ba
santimona1@gmail.com <santimona1@gmail.com>
2024-05-02 20:32:57 -0500

    Add create and list category and Notas

--- Models/Nota.cs
+++ Models/Nota.cs
@@ -7,13 +7,13 @@ namespace Backend2.Models
     public class Nota
     {
       [Key]
-      public int Id { get; set; }
-      public string? Nombre { get; set; }
+      public int id { get; set; }
+      public string? Titulo { get; set; }
       public string? Descripcion { get; set; }
-      public DateTime? FechaCreation { get; set; }
+      public DateTime? FechaCreacion { get; set; }
       public DateTime? HoraModificacion { get; set; }
       public string? Estado { get; set; }
-      public string? id_Categoria { get; set; }
+      public int? id_Categoria { get; set; }
 
 
     
